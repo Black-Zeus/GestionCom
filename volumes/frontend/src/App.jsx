e5315eba8001bc397e5832273f92a7ec2ef66c78
Victor Soto <blk.zeus@gmail.com>
2025-01-27 18:00:07 -0300

    feat(env): Soporte para carga correcta de variables de entorno en QA y PRD
    - Se actualizó el Dockerfile para incluir `ARG` y `ENV` correctamente en la etapa de construcción.
    - Se corrigió la configuración de los archivos `.env` para permitir la sobrescritura específica por ambiente (`qa`, `prd`).
    - Se añadió soporte para pasar variables de entorno a los servicios de Docker Compose, asegurando que `VITE_FRONTEND_ENV` y otras variables específicas se propaguen adecuadamente durante el build.
    - Se validó la funcionalidad en entornos QA y PRD con el flujo automatizado de `docker_tools`.
    
    Esta mejora asegura que las variables de entorno se carguen de manera consistente y precisa en todos los ambientes.

--- volumes/frontend/src/App.jsx
+++ volumes/frontend/src/App.jsx
@@ -1,5 +1,47 @@
+import React from 'react';
+
 export function App({ pca }) {
+  const environment = import.meta.env.VITE_FRONTEND_ENV || "Ambiente no especificado";
+
   return (
-    <h1>Hola Mundo</h1>
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <h1 style={styles.title}>Frontend React</h1>
+        <h2 style={styles.subtitle}>{environment}</h2>
+        <p style={styles.message}>Página de pruebas</p>
+      </div>
+    </div>
   );
 }
+
+const styles = {
+  container: {
+    backgroundColor: '#000', // Fondo negro
+    color: '#fff',           // Texto blanco
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'Arial, sans-serif',
+  },
+  card: {
+    textAlign: 'center',
+    padding: '20px',
+    borderRadius: '10px',
+    backgroundColor: '#222', // Fondo del card gris oscuro
+    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.5)',
+  },
+  title: {
+    fontSize: '2rem',
+    marginBottom: '10px',
+  },
+  subtitle: {
+    fontSize: '1.5rem',
+    marginBottom: '10px',
+    color: '#4CAF50', // Verde para resaltar el ambiente
+  },
+  message: {
+    fontSize: '1rem',
+    marginTop: '10px',
+  },
+};
